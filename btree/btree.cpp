97e170918a0de439b9fed602ec648ee00c1ee276
lufei <lufei@abc.com>
2024-03-04 18:32:42 +0800

    add btree

--- btree/btree.cpp
+++ btree/btree.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 /*
 一颗m阶的B树性质：
   1. 每个节点最多有m个子节点
@@ -7,3 +9,76 @@
   5. 每个节点至多包含2k-1个键
   6. 所有的叶子节点都在同一层
 */
+class BTreeNode {
+  int *keys; 
+  int t;
+  BTreeNode **C;
+  int n;
+  bool leaf;
+
+public:
+  BTreeNode(int _t, bool _leaf);
+
+  void traverse();
+  BTreeNode *search(int k);
+  // 前提条件：节点没满
+  void insertNonFull(int k);
+  void splitChild(int i, BTreeNode *y);
+
+  friend class BTree;
+};
+
+class BTree {
+  BTreeNode *root;
+  int t;
+
+public:
+  BTree(int _t) {
+    root = nullptr, t = _t;
+  }
+
+  void traverse() {
+    if (root != nullptr) root->traverse();
+  }
+
+  BTreeNode* search(int k) {
+    return (nullptr == root) ? nullptr : root->search(k);
+  }
+
+  void insert(int);
+};
+
+BTreeNode::BTreeNode(int _t, bool _leaf) {
+  t = _t;
+  leaf = _leaf;
+  
+  keys = new int[2 * t - 1];
+  C = new BTreeNode*[2 * t];
+
+  n = 0;
+}
+
+BTreeNode* BTreeNode::search(int k) {
+  int i = 0;
+  while (i < n && k > keys[i]) i++;
+
+  if (keys[i] == k) return this;
+
+  if (leaf) return nullptr;
+
+  return C[i]->search(k);
+}
+
+void BTreeNode::traverse() {
+  int i;
+  for (i = 0; i < n; i++) {
+    // 如果当前节点不是叶子节点，先遍历以C[i]为根的子树
+    if (!leaf) C[i]->traverse();
+    cout << " " << keys[i];
+  }
+  // 打印以最后一个孩子为根的子树
+  if (!leaf)
+    C[i]->traverse();
+}
+
+
